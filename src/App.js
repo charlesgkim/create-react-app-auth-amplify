c52eb8e51299e511916812c8792d655ba1126244
Charles Guanglim Kim <charles.g.kim@gmail.com>
2020-05-03 02:33:02 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -9,27 +9,7 @@ Amplify.configure(aws_exports);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        $(document).ready(function() {
-          $('#member').DataTable({
-            "ajax": {
-                "url": "https://0ik98nbwxb.execute-api.eu-west-2.amazonaws.com/dev/members",
-                "dataSrc": ""
-             },
-            "columns": [
-              { "data": "id" },
-              { "data": "password" },
-              { "data": "name" },
-              { "data": "tel" },
-              { "data": "mail" },
-              { "data": "position" },
-              { "data": "permission" },
-              { "data": "leaves" },
-              { "data": "note" }
-            ]
-          })
-        });
-    
+      <div className="App"> 
         <table id="member" class="display" style="width:100%">
           <thead>
               <tr>
