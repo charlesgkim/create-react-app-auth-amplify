f34722342b35d314ca9de2e2077bf98c61113b05
Charles Guanglim Kim <charles.g.kim@gmail.com>
2020-05-04 23:52:41 +0100

    Update Tbl.js

--- src/Tbl.js
+++ src/Tbl.js
@@ -6,6 +6,21 @@ $.DataTable = require('datatables.net');
 
 export class Tbl extends Component {
   componentDidMount() {
+    console.log(this.el);
+    this.$el = $(this.el);
+    this.$el.DataTable({
+    data: this.props.data,
+    columns: [
+      { "data": "id" },
+      { "data": "password" },
+      { "data": "name" },
+      { "data": "tel" },
+      { "data": "mail" },
+      { "data": "position" },
+      { "data": "permission" },
+      { "data": "leaves" },
+      { "data": "note" }
+    ]})    
   }
   componentWillUnmount() {
   }
