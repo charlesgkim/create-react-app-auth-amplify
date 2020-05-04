5f68b321cd41062f5c3cf2b7a3d9c8ba1f1b9c30
Charles Guanglim Kim <charles.g.kim@gmail.com>
2020-05-04 22:59:53 +0100

    Create Tbl.js

---
+++ src/Tbl.js
@@ -0,0 +1,16 @@
+import './css/jquery.dataTables.css';
+import React, { Component } from 'react';
+
+const $ = require('jquery');
+$.DataTable = require('datatables.net');
+
+export class Tbl extends Component {
+  componentDidMount() {
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    return <div>
+    </div>
+  }
+}
