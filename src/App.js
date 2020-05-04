322f5edbeb404c7bd8e3780ebf03cd8a8889a36f
Charles Guanglim Kim <charles.g.kim@gmail.com>
2020-05-04 03:25:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,41 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
+        $(document).ready(function() {
+          $('#member').DataTable({
+            "ajax": {
+                "url": "https://0ik98nbwxb.execute-api.eu-west-2.amazonaws.com/dev/members",
+                "dataSrc": ""
+             },
+            "columns": [
+              { "data": "id" },
+              { "data": "password" },
+              { "data": "name" },
+              { "data": "tel" },
+              { "data": "mail" },
+              { "data": "position" },
+              { "data": "permission" },
+              { "data": "leaves" },
+              { "data": "note" }
+            ]
+          })
+        });
+
+          <table id="member" class="display" style="width:100%">
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Password</th>
+                      <th>Name</th>
+                      <th>Tel</th>
+                      <th>Mail</th>
+                      <th>Position</th>
+                      <th>Permission</th>
+                      <th>Leaves</th>
+                      <th>Note</th>
+                  </tr>
+              </thead>
+          </table>    
         <header className="App-header">
           <pre>Learn React</pre>
         </header>
