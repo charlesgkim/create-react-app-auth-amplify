db75e84692a2c75fd30057e2fcdc1e41dd0c3c2c
Charles Guanglim Kim <charles.g.kim@gmail.com>
2020-05-05 01:38:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     );
   }
   
-  dataSet = [["Joshua","londonubf!23","Joshua Kim","07710 168455","JoshuaKim@sekyee.co.uk","Director","3","3/20",""],["Daniel","daniel123","Daniel Targain","07786 363054","DanielTargain@sekyee.co.uk","HR Manager","3","","Admin Skype Name/Password=daniel.targain/d*****!23\r\npaypal=targain@gmail.com/u*u**_1**\r\n\r\nProfile name  SaveCancel\r\n\r\nCalling channels\t 1 channels \t\r\nOutgoing calls\t Manage Credit   Manage Auto-recharge\t\r\nMaking regular calls to the US?\r\nSet up US minute bundles\t\r\nCaller ID \t Set up Caller ID\t\r\nIncoming calls\t+442071936844 Click to edit\t\r\ntargain.floral.designs\tView account details\r\nRemove account\t\r\nAdd a number or business account\r\n\r\nYour Skype Connect SIP password\r\nSkype Number: +44 20 8123 1338\r\nHere's the password for \"Targain Floral Design\" -- your Skype Connect SIP profile.\r\nSIP User: 99051000239476\r\nSkype Connect address: sip.skype.com\r\nUDP Port: 5060\r\nPassword: QSbH3M5FS7eZrb\r\nSkype"]]
+  dataSet = [["Joshua","londonubf!23","Joshua Kim","07710 168455","JoshuaKim@sekyee.co.uk","Director","3","3/20",""]]
 }
 
 export default withAuthenticator(App, true);
