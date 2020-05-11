a51d7f2f7ee90e07922e604b106991a7629bf789
Charles Kim <charles.g.kim@gmail.com>
2020-05-11 02:20:50 +0100

    11 may 2020 update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
 import React, { Component } from 'react';
 import {Tbl} from './Tbl';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { Auth, API } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+// import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
   
 Amplify.configure(aws_exports);
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "members",
+        endpoint: "https://0ik98nbwxb.execute-api.eu-west-2.amazonaws.com" // your API GW base url
+      }
+    ]
+  }
+});
 
 class App extends Component {
-
-  
   state = {
     loading: true,
     error: null,
     members: []
   }
 
-  componentDidMount () {
-      const url = "https://0ik98nbwxb.execute-api.eu-west-2.amazonaws.com/dev/members"
+  async componentDidMount () {
+      const apiName = "members";
+      const endPoint = "/dev/members";
 
-      fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        // data is parsed json object received from url
-        this.setState({ members: data, loading: false })
-      })
-      .catch((error) => {
-        // handle your errors here
+      let myInit = { 
+        headers: { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+      }
+      console.log(myInit)
+
+      await API.get(apiName ,endPoint, myInit)
+      .then(response => {
+        // data object received from api
+        console.log(response);
+        this.setState({ members: response, loading: false })
+      }).catch(error => {
         this.setState({ error, loading: false })
-        console.error(error)
-      })
+        console.log(JSON.stringify(error))
+      });
   }
   render() {
     const {
         loading,
         members
     } = this.state;
-    
-    var poolData = { UserPoolId: "eu-west-2_NyUIOKaje", ClientId: "v9sutffftmei0jsup5qjkmkd2" }
-    var token = null;
-    //var apigClientFactory = require('aws-api-gateway-client').default;
-    //var apiClient = apigClientFactory.newClient();
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-    userPool.getCurrentUser().getSession(function(err, session) { console.log(session.getIdToken().getJwtToken()); })
-    console.log(userPool);
     
     return (
       <div className="App"> 
